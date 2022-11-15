d0827737eeb15aa97113cd90585cc7f7d1823eb9
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-16 01:30:54 +0700

    init

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,55 @@
+// @ts-nocheck
+import { Button } from "antd";
+import React from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import constants from "../../constants";
+import { axiosPrivate } from "../../common/axiosPrivate";
+function Home() {
+  const navigate = useNavigate();
+
+  const queries = useQuery({
+    queryKey: "users",
+    queryFn: async () => {
+      const response = await axiosPrivate.get(
+        `${constants.apiConfig.ENDPOINT.profile}`
+      );
+      console.log("Response", response);
+      return response?.data || {};
+    },
+    onSuccess: (data) => {
+      if (data?.code === 200) {
+        console.log(data);
+      } else {
+        navigate("/login");
+      }
+    },
+    onError: (error) => {
+      console.log("Error", error);
+      navigate("/login");
+    }
+  });
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {queries.isLoading && <p>Loading...</p>}
+      {queries.isError && <p>Error</p>}
+      {queries?.data?.data && (
+        <div>
+          <p>Full name: {queries.data?.data?.fullName}</p>
+          <p>Email: {queries.data?.data?.email}</p>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("session");
+              navigate("/login");
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
---
+++ src/helpers/validate.ts
@@ -0,0 +1,16 @@
+import { object, string } from "yup";
+
+let registerSchema = object({
+  fullName: string().required("Full name is required"),
+  email: string().email("Email must be a valid email"),
+  password: string()
+    .min(8, "password must have at least 8 characters")
+    .required("password is required")
+});
+let loginSchema = object({
+  email: string().email("Email must be a valid email"),
+  password: string()
+    .min(8, "password must have at least 8 characters")
+    .required("password is required")
+});
+export { registerSchema, loginSchema };
