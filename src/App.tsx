bada746cb68dd178fa8ab7d2a0d428038c056ae8
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-18 02:16:35 +0700

    Use react-query instead of @tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Login from './pages/login'
