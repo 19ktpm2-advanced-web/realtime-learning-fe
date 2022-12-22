d65bebbd52d1e962b4ac090bc831acb34cc064ff
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-12-23 03:52:04 +0700

    Add style for heading and paragraph slide

--- src/components/slideContent/paragraph/ParagraphContent.tsx
+++ src/components/slideContent/paragraph/ParagraphContent.tsx
@@ -1,11 +1,12 @@
 import { IParagraphSlide } from 'interfaces'
+import styles from './styles.module.css'
 
 function ParagraphContent({ slide }: { slide: IParagraphSlide }) {
     console.log(slide)
     return (
-        <div className="heading-content">
-            <p>{slide.heading}</p>
-            <p>{slide.paragraph}</p>
+        <div className={styles.paragraphWrapper}>
+            <p className={styles.paragraphHeading}>{slide.heading}</p>
+            <p className={styles.paragraphContent}>{slide.paragraph}</p>
         </div>
     )
 }
