b3ad789ecf7e1482ac0c8593eef85b485ead74cd
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-12-24 17:25:29 +0700

    done notification in group if has presenting

--- src/components/notification-presenting/PresentingNotification.tsx
+++ src/components/notification-presenting/PresentingNotification.tsx
@@ -1,17 +1,23 @@
+import { CloseOutlined } from '@ant-design/icons'
 import { failureModal } from 'components/modals'
 import { IPresentation } from 'interfaces'
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import instance from 'service/axiosPrivate'
-import { generateInvitationLink } from 'utils'
+import { generatePresentationLink } from 'utils/presentation.util'
 import styles from './styles.module.css'
 
 const PresentingNotification = ({ presentationId }: { presentationId?: string }) => {
     const [presentation, setPresentation] = useState<IPresentation>({} as IPresentation)
+    const [presentationPath, setPresentationPath] = useState('')
+    const [presentationLink, setPresentationLink] = useState('')
+    const [show, setShow] = useState(true)
     const fetchPresentation = async () => {
         try {
             const result = await instance.get(`/presentation/get/${presentationId}`)
             if (result.status === 200) {
-                setPresentation(result.data.presentation)
+                console.log('result', result.data)
+                setPresentation(result.data)
             } else {
                 failureModal('Get presentation failed', result.statusText)
             }
@@ -24,17 +30,37 @@ const PresentingNotification = ({ presentationId }: { presentationId?: string })
             fetchPresentation()
         }
     }, [presentationId])
+    useEffect(() => {
+        if (
+            presentation &&
+            presentation.inviteCode &&
+            presentation.isPresenting &&
+            presentation.slideList
+        ) {
+            const currentSlide = presentation?.slideList?.[presentation?.currentSlide ?? 0]
+            if (currentSlide) {
+                const { url, path } = generatePresentationLink(
+                    presentation.inviteCode,
+                    currentSlide.type,
+                )
+                setPresentationPath(path)
+                setPresentationLink(url)
+            }
+        }
+    }, [presentation])
     return (
-        <div className={styles.container}>
-            <div className={styles.notification}>
-                <div className={styles.notificationTitle}>
-                    {presentation.inviteCode
-                        ? `Presentation is presenting ${generateInvitationLink(
-                              presentation.inviteCode,
-                          )}`
-                        : null}
+        <div className={styles.notificationContainer}>
+            {show && presentationPath && presentationLink && (
+                <div className={styles.notification}>
+                    <div className={styles.notificationText}>
+                        Presentation is presenting{' '}
+                        <Link to={presentationPath}>{presentationLink}</Link>
+                    </div>
+                    <div className={styles.notificationIcon}>
+                        <CloseOutlined onClick={() => setShow(false)} />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
